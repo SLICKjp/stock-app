861633277f780d06ac8f7bf359b8cff7678c9109
SLICKjp <47847649+SLICKjp@users.noreply.github.com>
2022-03-09 11:59:51 +0530

    Updated Hero Section

--- src/components/Herosection/Herosection.js
+++ src/components/Herosection/Herosection.js
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import './Herosection.css';
+import Pic from './pic1.jpg';
 const Herosection = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container1">
+        <div className="text-container">  
+        <h1 className="text-heading">Better Way To Invest</h1>
+        <p className="text-description">
+          Enjoy the feeling and experience of a better way to invest in
+          everything. No, more struggling, striving, or even trying. It's just
+          Better !
+        </p>
+        </div>
+        <div className="image-container">
+        <img src={Pic} className="design" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Herosection
+export default Herosection;
