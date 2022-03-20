8ce7e32c1b15cb899f5bdef06c6ff953d8bb5c2c
SLICKjp <47847649+SLICKjp@users.noreply.github.com>
2022-03-20 19:09:44 +0530

    "Added a new section"

--- src/components/Information/Info.js
+++ src/components/Information/Info.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import './info.css';
+import infopic1 from './info-pic1.jpg';
 
 const Info = () => {
     return (
         <>
          <div className="info-container">
               <div className="info-text-container">
-                  
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             
              <div className="info-image-container">
-           <img src="" alt=""/>    
+           <img src={infopic1} className="info-pic-1" alt=""/>    
              </div>
 
          </div>   
--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Herosection from '../Herosection/Herosection';
 import Itemcontainer from '../Items/Itemcontainer';
+import Info from '../Information/Info';
 
 const Home = () => {
     return (
         <div>
            <Herosection/>
            <Itemcontainer/>
+           <Info/>
         </div>
     )
 }
