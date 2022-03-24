882d42876098246a0b3a1add43887a4e865c7276
neehalhansoti4 <neehal.180410116029@gmail.com>
2022-03-24 12:17:12 +0530

    "Resolved import issue"

--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -12,11 +12,7 @@ const Home = () => {
            <Itemcontainer/>
            <Info/>
            <Information2/>
-<<<<<<< HEAD
           
-=======
-            {/* <Aboutus/> */}
->>>>>>> 1eb0eb5b8ba19551a16a594b7602eef7c71b825f
         </div>
     )
 }
